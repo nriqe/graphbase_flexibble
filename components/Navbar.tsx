0be3528f21b4f74bd4b27f3dbd0b004b58c631f5
Enrique Arias <enrique.arias@fractalservicios.pe>
2023-09-01 08:42:42 -0500

    feat: app flexibble

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,15 @@
-import { NavLinks } from "@/constants"
-import { link } from "fs"
+import { NavLinks } from "../constants";
 import Image from "next/image"
 import Link from "next/link"
 import React from 'react'
 import AuthProviders from "./AuthProviders"
+import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
+const Navbar = async () => {
 
-    const session = {};
+    const session = await getCurrentUser();
 
     return (
         <nav className="flexBetween navbar">
@@ -29,12 +31,12 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="flexCenter gap-4">
-            {session ? (
-                <>
-                    UserPhoto 
+            {session?.user ? (
+                <>                    
+                    <ProfileMenu session={session} />
                     <Link href="/create-project">
                         Share Work 
-                    </Link>
+                    </Link>                    
                 </>
                 ) : (
                     <AuthProviders />
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
+    },
+    serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
+}
 
 module.exports = nextConfig
